0052c03276e5a8036d09cbef378bef8b0b0d180d
harmantws <harman03tws@gmail.com>
2024-08-12 22:02:48 +0530

    update code

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ load_dotenv()
 app = FastAPI()
 
 API_KEY = os.environ['Youtube_API']
+# API_KEY='AIzaSyC6YvBJxONs5PGsMydo0WIAomC5mRzW3vU'
+
 SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search'
 
 
@@ -40,8 +42,46 @@ def search_youtube(query: str, language: str) -> dict:
         print(f"Unexpected error: {e}")
         return {"error": str(e)}
 
+
+# def contains_harmful_keywords(text: str, harmful_keywords: List[str]) -> bool:
+#     return any(keyword in text.lower() for keyword in harmful_keywords)
+
+# def contains_allowed_categories(text: str) -> bool:
+#     allowed_categories = [
+#         'story', 'motivational', 'poem', 'cartoon', 'educational', 'tutorial',
+#         'technology'
+#     ]
+#     text = text.lower()
+#     return any(category in text for category in allowed_categories)
+
+
+def filter_videos(videos: dict) -> List[Video]:
+    filtered_videos = []
+
+    for video in videos.get('items', []):
+        video_id = video['id']['videoId']
+        title = video['snippet']['title']
+        description = video['snippet']['description']
+        thumbnails = video['snippet']['thumbnails']
+
+        # if not contains_allowed_categories(title) and \
+        #    not contains_allowed_categories(description):
+        #     continue
+
+        # if not contains_harmful_keywords(title, harmful_keywords) and \
+        #    not contains_harmful_keywords(description, harmful_keywords):
+        filtered_videos.append(
+            Video(id=video_id,
+                  snippet=VideoSnippet(title=title,
+                                       description=description,
+                                       thumbnails=thumbnails)))
+
+    return filtered_videos
+
+
 def SafeSearchModel(query: str) -> str:
     api_key = os.environ['Gemini_API']
+    # api_key = 'AIzaSyD_VuOzd4s5D1633dtm2BdCNmUWjzbbxis'
     llm = ChatGoogleGenerativeAI(model="gemini-1.0-pro", api_key=api_key)
 
     prompt_template = PromptTemplate(
@@ -72,7 +112,6 @@ def SafeSearchModel(query: str) -> str:
     else:
         return 'Not Allowed'
 
-
 @app.get("/search", response_model=Union[List[Video], ErrorResponse])
 async def search_videos(
         query: str = Query(..., description="Search query for YouTube videos"),
@@ -80,6 +119,17 @@ async def search_videos(
             ...,
             description="Language code (e.g., 'hi' for Hindi, 'pa' for Punjabi)"
         )):
+
+    # harmful_keywords = [
+    #     'violence', 'abuse', 'assault', 'blood', 'torture', 'murder', 'fight',
+    #     'gore', 'self-harm', 'cruelty', 'drugs', 'alcohol', 'addiction',
+    #     'narcotics', 'overdose', 'hate', 'racism', 'sexism', 'sexy',
+    #     'pornography', 'explicit', 'sexual', 'adult content', 'nudity',
+    #     'dangerous', 'risky', 'illegal', 'criminal', 'sex', 'scam',
+    #     'harassment', 'bullying', 'threats', 'intimidation', 'trolling',
+    #     'depression', 'suicide', 'self-destruction', 'extremist', 'terrorism',
+    #     'radicalization', 'hate speech', 'danger'
+    # ]
 
     query = query.lower()
     safe_search_result = SafeSearchModel(query)
@@ -89,7 +139,10 @@ async def search_videos(
             "This search query is not allowed according to YouTube's policy.")
     else:
         search_results = search_youtube(query, language)
-        return search_results
+
+    safe_videos = filter_videos(search_results)
+    return safe_videos
+
 
 @app.get("/", response_class=HTMLResponse)
 async def read_index():
